25d7d8ffce3f99479931a05ec9e77a1afca9220d
Nikolay Petrov <nikolay.petrov@abv.bg>
2026-03-31 15:44:29 +0300

    add DataMgr to contain and manage data

---
+++ classes/DataMgr.py
@@ -0,0 +1,19 @@
+import json
+
+class DataMgr:
+    """Manages data from data.json file."""
+    def __init__(self, data_file):
+        with open(data_file, 'r') as f:
+            self.data = json.load(f)
+    
+    def get_project_by_id(self, project_id):
+        for p_id in self.data['projects']:
+            if p_id == project_id:
+                return self.data['projects'][p_id]
+        return None
+    
+    def get_coworker_by_id(self, coworker_id):
+        for c_id in self.data['co_workers']:
+            if c_id == coworker_id:
+                return self.data['co_workers'][c_id]
+        return None
